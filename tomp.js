5d08d0582cc82f4df15934808e4638ed82e74ec1
wildannss <49558726+wildannss@users.noreply.github.com>
2020-05-06 09:14:13 +0700

    migrate

--- tomp.js
+++ tomp.js
@@ -131,7 +131,7 @@ function auto_safeconvert() {
         }
         if (a_to_vh == false) {
             var encryptedUrl = Base64.encode(a_to_vi);
-            a_to_ve[i].href = "https://melekpajak.com/ads/?url=" + encryptedUrl;
+            a_to_ve[i].href = "https://blogspajak.com/ads/?url=" + encryptedUrl;
             a_to_ve[i].rel = "nofollow";
             a_to_vb++;
             a_to_vc += i + ":::" + a_to_ve[i].href + "\n";
